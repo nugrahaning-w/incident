4bceebf11952cc0d6e928e3045448057852b4c2a
Aji Nugrahaning Widhi <ajjinugrahaningwidhi@outlook.com>
2026-01-07 14:29:32 +0700

    Initial Commit

---
+++ incident/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  incident
+//
+//  Created by Aji Nugrahaning Widhi on 07/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
