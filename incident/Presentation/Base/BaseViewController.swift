328de75197fdd16a282f65a3b3a99175127f3782
Aji Nugrahaning Widhi <ajjinugrahaningwidhi@outlook.com>
2026-01-13 03:24:56 +0800

    BaseViewController leak aid (debug-only)

--- incident/Presentation/Base/BaseViewController.swift
+++ incident/Presentation/Base/BaseViewController.swift
@@ -14,6 +14,12 @@ class BaseViewController<VM: BaseViewModel>: UIViewController {
     }
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
+    deinit {
+        #if DEBUG
+        print("[DEINIT] \(String(describing: type(of: self)))")
+        #endif
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
